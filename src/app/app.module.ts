fd4e158626ba8402609f9c6c9e8322f8468a9e2a
AmanyMo <amany123mohamed.am@gmail.com>
2022-05-05 17:08:00 +0200

    first commit to add structure of angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeBooksComponent } from './recipe-books/recipe-books.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeBooksComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule
